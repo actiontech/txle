5fa4883b6243f27b7b59f1cf0d64b6f2b67fa9b0
yanalong <yanalong@actionsky.com>
2018-08-21 14:19:46 +0800

    1.文件【omega/omega-context/src/main/java/org/apache/servicecomb/saga/omega/context/CompensationContext.java】处理自动补偿接口类实例化问题 2.文件【omega/omega-spring-starter/src/main/java/org/apache/servicecomb/saga/omega/spring/EnableOmega.java】引入自动补偿工程的配置文件AutoCompensableAspectConfig 3.文件【omega/omega-spring-tx/src/main/java/org/apache/servicecomb/saga/omega/transaction/spring/CompensableMethodCheckingCallback.java】处理自动补偿接口类实例化问题

--- omega/omega-spring-starter/src/main/java/org/apache/servicecomb/saga/omega/spring/EnableOmega.java
+++ omega/omega-spring-starter/src/main/java/org/apache/servicecomb/saga/omega/spring/EnableOmega.java
@@ -22,13 +22,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.context.annotation.Import;
-
+import org.apache.servicecomb.saga.omega.transaction.AutoCompensableAspectConfig;
 import org.apache.servicecomb.saga.omega.transaction.spring.TransactionAspectConfig;
+import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Import({OmegaSpringConfig.class, TransactionAspectConfig.class})
+@Import({OmegaSpringConfig.class, TransactionAspectConfig.class, AutoCompensableAspectConfig.class})
+
+//@EntityScan(basePackages = {"com.gannalyo.saga.user.entity"})
+//@EnableJpaRepositories(basePackages = {"com.gannalyo.saga.user.repository"})
+//@EntityScan(basePackages = {"org.apache.servicecomb.saga.omega.transaction.repository.entity"})
+//@EnableJpaRepositories(basePackages = {"org.apache.servicecomb.saga.omega.transaction.repository"})
+//@EntityScan(basePackages = {"org.apache.servicecomb.saga.omega.transaction.repository.entity", "com.gannalyo.saga.user.entity"})
+//@EnableJpaRepositories(basePackages = {"org.apache.servicecomb.saga.omega.transaction.repository", "com.gannalyo.saga.user.repository"})
 /**
  * Indicates create the OmegaContext and inject it into the interceptors
  * to pass the transactions id across the application.
