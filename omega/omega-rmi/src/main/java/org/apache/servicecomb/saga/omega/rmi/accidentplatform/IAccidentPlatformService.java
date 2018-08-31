57690e38be8c4ff538feffce07dea206a4907816
Gannalyo <javajob126email@126.com>
2018-08-31 14:06:27 +0800

    添加新工程omega-rmi，该工程主要提供对外API接口。

---
+++ omega/omega-rmi/src/main/java/org/apache/servicecomb/saga/omega/rmi/accidentplatform/IAccidentPlatformService.java
@@ -0,0 +1,12 @@
+package org.apache.servicecomb.saga.omega.rmi.accidentplatform;
+
+/**
+ * Accident Platform Interface.
+ * 
+ * @author Gannalyo
+ * @since 2018-08-27
+ */
+public interface IAccidentPlatformService {
+	// Information to accident platform. 
+	public boolean test(String msg);
+}
