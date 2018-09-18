a3798b3b80da87cfb31fa74f2a5425d0e9e28a5c
yanalong <yanalong@actionsky.com>
2018-09-18 13:22:05 +0800

    编写全局事物终止RestApi接口； 整理暂停、继续、终止代码；

--- alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/AdditionalEventType.java
+++ alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/AdditionalEventType.java
@@ -1,6 +1,7 @@
 package org.apache.servicecomb.saga.alpha.core;
 
 public enum AdditionalEventType {
+	// It's a pity it could not extend the enum 'EventType'.
 	SagaPausedEvent,
 	SagaContinuedEvent,
 	SagaAutoContinuedEvent
--- alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/restapi/UtxRestApi.java
+++ alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/restapi/UtxRestApi.java
@@ -1,5 +1,7 @@
 package org.apache.servicecomb.saga.alpha.server.restapi;
 
+import static org.apache.servicecomb.saga.common.EventType.TxAbortedEvent;
+
 import java.util.List;
 import java.util.Map;
 
@@ -66,65 +68,96 @@ public class UtxRestApi {
 	/**
 	 * To pause specified global transaction by identify.
 	 * 
-	 * @param globalTxId identify for global transaction
+	 * @param txEventId identify for global transaction
 	 * @return
 	 */
-	@GetMapping("/pauseGlobalTransactionById/{globalTxId}")
-	public String pauseGlobalTransactionById(@PathVariable long globalTxId) {
-		return pauseGlobalTransactionById(globalTxId, 0);
+	@GetMapping("/pauseGlobalTransactionById/{txEventId}")
+	public String pauseGlobalTransactionById(@PathVariable long txEventId) {
+		return pauseGlobalTransactionById(txEventId, 0);
 	}
 
 	/**
 	 * To pause specified global transaction by identify.
 	 * 
-	 * @param globalTxId identify for global transaction
+	 * @param txEventId identify for global transaction
 	 * @param pausePeriod The global transaction will continue to run in 'pausePeriod' seconds automatically. It will not continue to if the value of its pausePeriod is 0. The former is the paused must be true;
 	 * @return
 	 */
-	@GetMapping("/pauseGlobalTransactionById/{globalTxId}/{pausePeriod}")
-	public String pauseGlobalTransactionById(@PathVariable long globalTxId, @PathVariable int pausePeriod) {
+	@GetMapping("/pauseGlobalTransactionById/{txEventId}/{pausePeriod}")
+	public String pauseGlobalTransactionById(@PathVariable long txEventId, @PathVariable int pausePeriod) {
 		if (pausePeriod < 0) {
 			pausePeriod = 0;
 		}
-		LOG.debug("Executing the 'pauseGlobalTransactionById' API - globalTxId [{}], pausePeriod [{}].", globalTxId, pausePeriod);
-		return pauseOrContinueGlobalTransactionById(globalTxId, pausePeriod, AdditionalEventType.SagaPausedEvent, "pause");
+		String ip_port = request.getRemoteAddr() + ":" + request.getRemotePort();
+		LOG.debug("Executing the 'pauseGlobalTransactionById' API - txEventId [{}], pausePeriod [{}], ip:port [{}].", txEventId, pausePeriod, ip_port);
+		
+		return saveOperationTxEventWithVerification(txEventId, pausePeriod, 0, "pause", ip_port);
 	}
 
-	@GetMapping("/continueGlobalTransactionById/{globalTxId}")
-	public String continueGlobalTransactionById(@PathVariable long globalTxId) {
-		LOG.debug("Executing the 'continueGlobalTransactionById' API - globalTxId [{}].", globalTxId);
-		return pauseOrContinueGlobalTransactionById(globalTxId, 0, AdditionalEventType.SagaContinuedEvent, "continue");
+	@GetMapping("/continueGlobalTransactionById/{txEventId}")
+	public String continueGlobalTransactionById(@PathVariable long txEventId) {
+		String ip_port = request.getRemoteAddr() + ":" + request.getRemotePort();
+		LOG.debug("Executing the 'continueGlobalTransactionById' API - txEventId [{}], ip:port [{}].", txEventId, ip_port);
+		
+		return saveOperationTxEventWithVerification(txEventId, 0, 1, "continue", ip_port);
 	}
 	
-	private String pauseOrContinueGlobalTransactionById(long globalTxId, int pausePeriod, AdditionalEventType additionalEventType, String operation) {
-		boolean pauseOperation = additionalEventType.equals(AdditionalEventType.SagaPausedEvent);
-		TxEvent txEvent = eventRepository.findOne(globalTxId);
-		if (txEvent == null) {
-			return "Fail to " + operation + " - could not find transaction by globalTxId [" + globalTxId + "].";
-		}
+	@GetMapping("/terminateGlobalTransactionById/{txEventId}")
+	public String terminateGlobalTransactionById(@PathVariable long txEventId) {
+		String ip_port = request.getRemoteAddr() + ":" + request.getRemotePort();
+		LOG.debug("Executing the 'terminateGlobalTransactionById' API - txEventId [{}], ip:port [{}].", txEventId, ip_port);
 		
+		return saveOperationTxEventWithVerification(txEventId, 0, -1, "terminate", ip_port);
+	}
+	
+	private String saveOperationTxEventWithVerification(long txEventId, int pausePeriod, int operation, String operationDesc, String ip_port) {
+		TxEvent txEvent = eventRepository.findOne(txEventId);
+		if (txEvent == null) {
+			return "Fail to " + operationDesc + " - could not find transaction by txEventId [" + txEventId + "].";
+		}
+		String globalTxId = txEvent.globalTxId();
+
 		// To verify if current transaction had been over.
-		List<TxEvent> endedTransactions = eventRepository.findTransactions(txEvent.globalTxId(), EventType.SagaEndedEvent.name());
+		List<TxEvent> endedTransactions = eventRepository.findTransactions(txEvent.globalTxId(),
+				EventType.SagaEndedEvent.name());
 		if (endedTransactions != null && !endedTransactions.isEmpty()) {
-			return "Fail to " + operation + " - current transaction was already over, globalTxId [" + txEvent.globalTxId() + "].";
+//			return "Fail to " + operationDesc + " - current transaction was already over, globalTxId [" + globalTxId + "].";
 		}
 
-	    // To verify if current transaction was suspended.
-		boolean isPaused = txConsistentService.isGlobalTxPaused(txEvent.globalTxId());
-		// The pause operation will not be executed when it is already paused.
-		if (pauseOperation && isPaused) {
-			return "Fail to " + operation + " - current transaction was suspended, globalTxId [" + txEvent.globalTxId() + "].";
+		String eventTypeName = null;
+		// 0 - pause, 1 - continue, -1 - terminate
+		switch (operation) {
+			case 0:
+				// To verify if current transaction was suspended.
+				boolean isPaused = txConsistentService.isGlobalTxPaused(globalTxId);
+				// The pause operation will not be executed when it is already paused.
+				if (isPaused) {
+					return "Fail to " + operation + " - current transaction was suspended, globalTxId [" + globalTxId + "].";
+				}
+				eventTypeName = AdditionalEventType.SagaPausedEvent.name();
+				break;
+			case 1:
+				// To verify if current transaction was suspended.
+				isPaused = txConsistentService.isGlobalTxPaused(globalTxId);
+				// The continue operation will not be executed when it is already not paused.
+				if (!isPaused) {
+					return "Fail to " + operation + " - current transaction was not suspended yet, globalTxId [" + globalTxId + "].";
+				}
+				eventTypeName = AdditionalEventType.SagaContinuedEvent.name();
+				break;
+			case -1:
+				List<TxEvent> abortedTxEventList = eventRepository.findTransactions(globalTxId, TxAbortedEvent.name());
+				if (abortedTxEventList != null && !abortedTxEventList.isEmpty()) {
+					return "Fail to " + operationDesc + " - current transaction was already aborted, globalTxId [" + globalTxId + "].";
+				}
+				eventTypeName = EventType.TxAbortedEvent.name();
+				break;
+			default:
+				break;
 		}
-		
-		// The continue operation will not be executed when it is already not paused.
-		if (!pauseOperation && !isPaused){
-			return "Fail to " + operation + " - current transaction was not suspended yet, globalTxId [" + txEvent.globalTxId() + "].";
-		}
-		
-		String ip_port = request.getRemoteAddr() + ":" + request.getRemotePort();
-		
-		eventRepository.save(new TxEvent(ip_port, ip_port, txEvent.globalTxId(), txEvent.localTxId(), txEvent.parentTxId(), additionalEventType.name(), "", pausePeriod, "", 0, null));
-		
+
+		eventRepository.save(new TxEvent(ip_port, ip_port, globalTxId, txEvent.localTxId(), txEvent.parentTxId(), eventTypeName, "", pausePeriod, "", 0, null));
+
 		return "ok";
 	}
 
