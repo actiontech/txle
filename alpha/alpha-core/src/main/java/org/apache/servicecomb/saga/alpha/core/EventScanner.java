7fa0712b4c4ce206eefb9fef08dde44c511c592d
yanalong <yanalong@actionsky.com>
2018-11-16 14:19:58 +0800

    修复issue：http://10.186.18.21/utx/utx/issues/1、http://10.186.18.21/utx/utx/issues/4

--- alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/EventScanner.java
+++ alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/EventScanner.java
@@ -187,7 +187,7 @@ public class EventScanner implements Runnable {
         timeout.parentTxId(),
         TxAbortedEvent.name(),
         "",
-        "",
+        timeout.category(),
         ("Transaction timeout").getBytes());
   }
 
