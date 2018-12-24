c2f7fd2c9062ff5791d281e50e5481db287b47b9
yanalong <yanalong@actionsky.com>
2018-12-24 10:41:49 +0800

    调整差错平台接口参数。主要增加差错类型、全局事务标识、子事务标识。 后续待正式集成差错平台时，再进一步调整该接口。

---
+++ pack-common/src/main/java/org/apache/servicecomb/saga/common/rmi/accidentplatform/AccidentType.java
@@ -0,0 +1,12 @@
+package org.apache.servicecomb.saga.common.rmi.accidentplatform;
+
+/**
+ * An enum for the accident type.
+ *
+ * @author Gannalyo
+ * @date 2018/12/13
+ */
+public enum AccidentType {
+    ROLLBACK_ERROR,
+    SEND_MESSAGE_ERROR;
+}
