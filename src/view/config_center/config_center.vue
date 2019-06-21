bf5f8fb5c1b8d65f9a39d62c6973a81a60aa2566
Anny <404928130@qq.com>
2019-06-21 21:54:10 +0800

    Initial commit

---
+++ src/view/config_center/config_center.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <div style>
+      <el-button-group>
+        <el-button type="primary" size="small" icon="el-icon-plus">新增</el-button>
+        <el-button type="primary" size="small" icon="el-icon-finished">保存</el-button>
+        <el-button type="danger" size="small" icon="el-icon-minus">删除</el-button>
+      </el-button-group>
+      <el-input
+        placeholder="请输入内容"
+        style="width:200px; float:right;"
+        prefix-icon="el-icon-search"
+        size="small"
+      ></el-input>
+    </div>
+    <el-table
+      :data="tableData"
+      style="width: 100%; margin-top:10px; margin-bottom: 20px;"
+      row-key="id"
+      border
+      stripe
+      default-expand-all
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+    >
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+    <div style="text-align:right;">
+      <el-pagination hide-on-single-page background layout="prev, pager, next" :total="1000"></el-pagination>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          children: [
+            {
+              id: 31,
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄"
+            },
+            {
+              id: 32,
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄"
+            }
+          ]
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+      tableData1: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          hasChildren: true
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
+  },
+  methods: {
+    load(tree, treeNode, resolve) {
+      setTimeout(() => {
+        resolve([
+          {
+            id: 31,
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          },
+          {
+            id: 32,
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          }
+        ]);
+      }, 1000);
+    }
+  }
+};
+</script>
