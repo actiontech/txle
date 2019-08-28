098d71b9ef56f297e3c382b41a4e4fa42fde5d76
yanalong <yanalong@actionsky.com>
2019-08-28 14:16:30 +0800

    Update copyright. /*  * Copyright (c) 2018-2019 ActionTech.  * based on code by ServiceComb Pack CopyrightHolder Copyright (C) 2018,  * License: http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0 or higher.  */

--- omega/omega-autocompensation/src/main/java/org/apache/servicecomb/saga/omega/transaction/annotations/AutoCompensable.java
+++ omega/omega-autocompensation/src/main/java/org/apache/servicecomb/saga/omega/transaction/annotations/AutoCompensable.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018-2019 ActionTech.
+ * based on code by ServiceComb Pack CopyrightHolder Copyright (C) 2018,
+ * License: http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0 or higher.
+ */
+
 package org.apache.servicecomb.saga.omega.transaction.annotations;
 
 import java.lang.annotation.ElementType;
