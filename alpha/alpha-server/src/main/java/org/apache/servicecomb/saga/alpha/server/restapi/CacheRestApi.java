d1d8dd29e6c62e7185f32b645ce022a0a799000b
Gannalyo <javajob126email@126.com>
2019-08-28 13:55:59 +0800

    Exclude the current server when refresh distributed cache. If it's the current server, then call local function directly.

--- alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/restapi/CacheRestApi.java
+++ alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/restapi/CacheRestApi.java
@@ -3,7 +3,10 @@ package org.apache.servicecomb.saga.alpha.server.restapi;
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.Response;
 import com.ecwid.consul.v1.agent.model.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.invoke.MethodHandles;
+import java.net.InetAddress;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Map;
@@ -19,6 +24,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class CacheRestApi {
+    private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final ConcurrentHashMap<String, Boolean> configCacheMap = new ConcurrentHashMap<>();
 
     @Autowired
@@ -26,6 +32,9 @@ public class CacheRestApi {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${spring.server.port:8090}")
+    private int serverPort;
 
     public Boolean get(String key) {
         return configCacheMap.get(key);
@@ -56,28 +65,42 @@ public class CacheRestApi {
         if (cacheKV != null) {
             configCacheMap.remove(cacheKV.split(",")[0]);
             if (configCacheMap.isEmpty()) {
-                // 释放内存
                 configCacheMap.clear();
             }
         }
     }
 
     public void refreshDistributedConfigCache(String cacheKey, Boolean cacheValue, String restApi) {
-        Response<Map<String, Service>> agentServices = consulClient.getAgentServices();
-        if (agentServices != null) {
-            Map<String, Service> serviceMap = agentServices.getValue();
-            if (serviceMap != null && !serviceMap.isEmpty()) {
-                Set<String> ipPortSet = new HashSet<>();
-                serviceMap.keySet().forEach(key -> {
-                    Service service = serviceMap.get(key);
-                    String ipPort = service.getAddress() + ":" + service.getPort();
-                    if (!ipPortSet.contains(ipPort)) {
-                        ipPortSet.add(ipPort);
-                        HttpEntity<String> entity = new HttpEntity<>(cacheKey + "," + cacheValue);
-                        restTemplate.exchange("http://" + ipPort + restApi, HttpMethod.POST, entity, Boolean.class);
-                    }
-                });
+        try {
+            Response<Map<String, Service>> agentServices = consulClient.getAgentServices();
+            if (agentServices != null) {
+                Map<String, Service> serviceMap = agentServices.getValue();
+                if (serviceMap != null && !serviceMap.isEmpty()) {
+                    String currentHostPort = InetAddress.getLocalHost().getHostName() + ":" + serverPort;
+                    Set<String> ipPortSet = new HashSet<>();
+                    serviceMap.keySet().forEach(key -> {
+                        Service service = serviceMap.get(key);
+                        String ipPort = service.getAddress() + ":" + service.getPort();
+                        if (!ipPortSet.contains(ipPort)) {
+                            ipPortSet.add(ipPort);
+
+                            if (currentHostPort.equals(ipPort)) {
+                                if ("/putConfigCache".equals(restApi)) {
+                                    putConfigCache(cacheKey + "," + cacheValue);
+                                } else if ("/removeConfigCache".equals(restApi)) {
+                                    removeConfigCache(cacheKey + "," + cacheValue);
+                                }
+                            } else {
+                                HttpEntity<String> entity = new HttpEntity<>(cacheKey + "," + cacheValue);
+                                log.error("Calling http://" + ipPort + restApi);
+                                restTemplate.exchange("http://" + ipPort + restApi, HttpMethod.POST, entity, Boolean.class);
+                            }
+                        }
+                    });
+                }
             }
+        } catch (Exception e) {
+            log.error("Failed to execute method 'refreshDistributedConfigCache'.", e);
         }
     }
 
